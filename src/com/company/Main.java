4551ed94b4b24a3194888cbae55ccd3b0a99402c
Evgeniy Vasiliev <ju5tc4t@gmail.com>
2018-03-16 20:49:00 +0300

    Cool main function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stringen st = new Stringen(100, 202);
+        st.start();
+
+        Scanner in = new Scanner(System.in);
+        String line;
+        while (!(line = in.nextLine()).equals("exit")) {
+            if (line.equals("Как закрыть долги?")) {
+                System.out.println(st.string);
+            }
+            else {
+                System.out.println(line);
+            }
+        }
     }
 }
