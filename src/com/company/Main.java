e7875c64687d5444bf474d1092c5f7d56732e57e
kinoooshnik <sasha.semiletow2011@yandex.ru>
2018-03-16 20:20:45 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,5 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        MainActivity a = new MainActivity();
-        a.show();
-        //MyClass myClass = new MyClass(20);
-        //myClass.show();
-        //Scanner in = new Scanner(System.in);
-        //String s = in.next();
-        //System.out.println(s);
-    }
-}
-
-class MyClass {
-    private int a;
-    public MyClass(int a)
-    {
-        this.a = a;
-    }
-
-    public void show()
-    {
-        System.out.println(this.a);
     }
 }
