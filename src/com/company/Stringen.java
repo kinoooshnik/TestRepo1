82f6ab150cb7d8d7a21280c100ed55ada65f6bf1
JohnFinn <dz4tune@gmail.com>
2018-03-16 20:30:00 +0300

    empty Stringen class

---
+++ src/com/company/Stringen.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Stringen extends Thread {
+    int start;
+    int end;
+    String string;
+
+    public Stringen(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+
+
+    public void run(){
+
+    }
+}
